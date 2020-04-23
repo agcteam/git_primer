f98390226fc13691f52c1b3e200360542b6bb8c3
Juan Felipe Ortiz <juanfortizq@gmail.com>
2020-04-23 15:35:35 +0900

    add cool script

---
+++ practice/cool_script.py
@@ -0,0 +1,11 @@
+from time import sleep
+# This is an awesome script that does many things
+user = input("Please type your name and press enter: ")
+
+print(f"Hello, dear {user}")
+sleep(3)
+print("Prepare yourself to be amazed!!")
+sleep(3)
+print("Here it goes!!!")
+sleep(3)
+pint("Tadaaaa!")
