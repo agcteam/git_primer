a917c032e1aafc1177981072361c49d8e015cfd5
Juan Felipe Ortiz <juanfortizq@gmail.com>
2020-04-24 10:26:29 +0900

    Fix python <3.5

--- practice/cool_script.py
+++ practice/cool_script.py
@@ -2,7 +2,7 @@ from time import sleep
 # This is an awesome script that does many things
 user = input("Please type your name and press enter: ")
 
-print(f"Hello, dear {user}")
+print("Hello, dear {}".format(user))
 sleep(3)
 print("Prepare yourself to be amazed!!")
 sleep(3)
